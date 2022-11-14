002ab3c1d30ae0949cb7e905ab9132d187d355d9
janrdz <rodz.abdiel17@gmail.com>
2022-11-14 08:39:57 -0400

    Exercise 7 using Math.sqrt and Math.pow

---
+++ workspace/operators/quadraticFormula.java
@@ -0,0 +1,30 @@
+package operators;
+
+import java.util.Scanner;
+
+public class quadraticFormula {
+	
+	public static void main(String[] args)
+	{
+		Scanner input = new Scanner(System.in);
+		
+		double root1, root2, b, a, c;
+		
+		System.out.print("Enter the value of a: -> ");
+		a = input.nextDouble();
+		
+		System.out.print("Enter the value of b: -> ");
+		b = input.nextDouble();
+		
+		System.out.print("Enter the value of c: -> ");
+		c = input.nextDouble();
+		input.close();
+		
+		root1 = (-b + Math.sqrt(Math.pow(b, 2) - 4 * (a * c)) / 2 * a);
+		root2 = (-b - Math.sqrt(Math.pow(b, 2) - 4 * (a * c)) / 2 * a);
+		
+		System.out.print("The result of Root 1 is: " + root1);
+		System.out.print("\nThe result of Root 2 is: " + root2);
+		
+	}
+}
